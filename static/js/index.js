515d2d6117fa013d7a4e9b9089a88085b251b5cf
AcerousKylin <w1027505714@gmail.com>
2025-05-15 19:53:27 +0800

    Using unified bootstrap style Fine-tune some layout and style

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,10 @@
 async function preloadGraphData() {
     try {
         const response = await fetch('/graph/api/', { headers: { 'X-Requested-With': 'XMLHttpRequest' } });
-
         if (!response.ok) {
             console.error("Preload failed - Status:", response.status);
             return;
         }
-
         const data = await response.json();
         sessionStorage.setItem('graphData', JSON.stringify(data)); // 存储数据
         console.log("Graph data preloaded!");
